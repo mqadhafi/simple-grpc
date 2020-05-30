5e59a4a118b1a293e5f7ddb1071d2a80aae39985
Dhafi <mqadhafi@live.com>
2020-05-30 22:53:56 +0700

    Add proto files on console app

--- CreditRatingClient.Cli/Program.cs
+++ CreditRatingClient.Cli/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using CreditRatingService;
+using Grpc.Net.Client;
 
 namespace CreditRatingClient.Cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serverAddress = "https://localhost:5001";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // The following statement allows you to call insecure services. To be used only in development environments.
+                AppContext.SetSwitch(
+                    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                serverAddress = "http://localhost:5000";
+            }
+
+            var channel = GrpcChannel.ForAddress(serverAddress);
+            var client = new CreditRatingCheck.CreditRatingCheckClient(channel);
+            var creditRequest = new CreditRequest { CustomerId = "id0201", Credit = 7000 };
+            var reply = await client.CheckCreditRequestAsync(creditRequest);
+
+            Console.WriteLine($"Credit for customer {creditRequest.CustomerId} {(reply.IsAccepted ? "approved" : "rejected")}!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
--- CreditRatingService/Program.cs
+++ CreditRatingService/Program.cs
@@ -1,4 +1,6 @@
+using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace CreditRatingService
@@ -13,10 +15,18 @@ namespace CreditRatingService
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+           Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                   {
+                       webBuilder.ConfigureKestrel(options =>
+                       {
+                           // Setup a HTTP/2 endpoint without TLS.
+                           options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+                       });
+                   }
+                   webBuilder.UseStartup<Startup>();
+               });
     }
 }
