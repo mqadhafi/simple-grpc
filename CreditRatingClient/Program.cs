4306f0e7aebb78221321826d58c3f8fc405290d7
Dhafi <mqadhafi@live.com>
2020-05-30 17:35:35 +0700

    Add new console project

---
+++ CreditRatingClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditRatingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
