8f33c49432220471c9b4765a0db50ac1adda3af5
Dhafi <mqadhafi@live.com>
2020-05-30 23:07:31 +0700

    Call greeter on api client

---
+++ CreditRatingClient.Api/Controllers/GreetersController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using CreditRatingService;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditRatingClient.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetersController : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+            Greeter.GreeterClient greetingClient = new Greeter.GreeterClient(channel);
+            HelloRequest greeterRequest = new HelloRequest { Name = name };
+            HelloReply greeterResponse = await greetingClient.SayHelloAsync(greeterRequest);
+            return Ok(greeterResponse.Message);
+        }
+    }
+}
